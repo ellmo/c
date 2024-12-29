570218916c407761fc08557a656600bf00f8c03c
Jakub Żuchowski <ellmunzai@gmail.com>
2024-12-29 19:53:28 +0100

    simplifying loops by initializing the iterator inside them

--- lcthw/ex14/ex14.c
+++ lcthw/ex14/ex14.c
@@ -21,9 +21,7 @@ int main(int argc, char *argv[])
 
 void print_arguments(int argc, char *argv[])
 {
-    int i = 0;
-
-    for (i = 0; i < argc; i++)
+    for (int i = 0; i < argc; i++)
     {
         print_letters(argv[i]);
     }
@@ -31,9 +29,7 @@ void print_arguments(int argc, char *argv[])
 
 void print_letters(char arg[])
 {
-    int i = 0;
-
-    for (i = 0; arg[i] != '\0'; i++)
+    for (int i = 0; arg[i] != '\0'; i++)
     {
         char ch = arg[i];
 
