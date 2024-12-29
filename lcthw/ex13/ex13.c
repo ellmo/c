ab3e56656cd4b12161682a8fab8cea6b7b44a0da
Jakub Żuchowski <ellmunzai@gmail.com>
2024-12-29 18:05:25 +0100

    example 13, demonstrating loops, strings, pointers (a bit)

---
+++ lcthw/ex13/ex13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    /*
+        Q: Go through each string in argv. Why am I skipping argv[0]?
+
+        A: Becasue argv[0] is always the name of the program (eg. "./13")
+    */
+   for (i = 1; i < argc; i++) {
+        printf("arg %d: %s\n", i, argv[i]);
+    }
+
+    // let's make our own array of strings
+    char *states[] = {
+        "California", "Oregon",
+        "Washington", "Texas"
+    };
+
+    int num_states = 4;
+
+    for (i = 0; i < num_states; i++) {
+        printf("state %d: %s\n", i, states[i]);
+    }
+
+    return 0;
+}
